a509bce9d89f940cdf29fda461acfe9f32118eee
feitrackr-hub <feitrackr@gmail.com>
2026-06-03 22:36:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-server.js - with /leaderboard endpoint
 const express = require('express');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const cors = require('cors');
